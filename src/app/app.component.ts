2702dcffa218c598a912fa5c8d4ea73d8ac2d181
Bram Borggreve <beeman@beeman.nl>
2024-03-21 17:13:19 +0000

    feat: add cluster service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,58 @@
-import { Component } from '@angular/core'
+import { JsonPipe } from '@angular/common'
+import { Component, inject } from '@angular/core'
 import { RouterOutlet } from '@angular/router'
+import { ClusterService } from './cluster/cluster.service'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, JsonPipe],
   template: `
     <div>
       <header>
-        HEADER
+
+        <button (click)="addCluster()">Add Cluster</button>
+        <button (click)="addClusterErr()">Add Cluster Err</button>
       </header>
       <main>
-        <router-outlet />
+        <table border='1' cellpadding="10">
+
+          <tbody>
+            @for (cluster of clusterService.clusters; track cluster.name) {
+              <tr>
+                <td>{{ cluster.endpoint }}</td>
+                <td>{{ cluster.name }}</td>
+                <td>
+                  <button (click)="clusterService.deleteCluster(cluster)">Delete</button>
+                  <button (click)="clusterService.setCluster(cluster)">Set</button>
+                </td>
+              </tr>
+            } @empty {
+              No clusters
+            }
+          </tbody>
+        </table>
+
+
+        <pre>cluster: {{clusterService.cluster | json }}</pre>
+        <pre>clusters: {{clusterService.clusters | json }}</pre>
       </main>
-      <footer>
-        FOOTER
-      </footer>
+
     </div>
   `,
 })
 export class AppComponent {
+  clusterService = inject(ClusterService)
 
+
+  addCluster() {
+    const name = `test-${Date.now()}`
+    this.clusterService.addCluster({ endpoint: 'http://localhost:8899', name, })
+  }
+
+  addClusterErr() {
+    const name = `test-${Date.now()}`
+    this.clusterService.addCluster({ endpoint: 'localhost:8899', name, })
+  }
 
 }
---
+++ src/app/cluster/cluster.service.ts
@@ -0,0 +1,96 @@
+import { inject, Injectable } from '@angular/core'
+import { clusterApiUrl, Connection } from '@solana/web3.js'
+import { LocalStorageService } from 'ngx-localstorage'
+
+export interface Cluster {
+  name: string
+  endpoint: string
+  network?: ClusterNetwork
+  active?: boolean
+}
+
+export enum ClusterNetwork {
+  Mainnet = 'mainnet-beta',
+  Testnet = 'testnet',
+  Devnet = 'devnet',
+  Custom = 'custom',
+}
+// By default, we don't configure the mainnet-beta cluster
+// The endpoint provided by clusterApiUrl('mainnet-beta') does not allow access from the browser due to CORS restrictions
+// To use the mainnet-beta cluster, provide a custom endpoint
+export const defaultClusters: Cluster[] = [
+  {
+    name: 'devnet',
+    endpoint: clusterApiUrl('devnet'),
+    network: ClusterNetwork.Devnet,
+  },
+  { name: 'local', endpoint: 'http://localhost:8899' },
+  {
+    name: 'testnet',
+    endpoint: clusterApiUrl('testnet'),
+    network: ClusterNetwork.Testnet,
+  },
+]
+
+const STORAGE_KEY_CLUSTER = 'solana-cluster'
+const STORAGE_KEY_CLUSTERS = 'solana-clusters'
+
+@Injectable({ providedIn: 'root' })
+export class ClusterService {
+  readonly storage = inject(LocalStorageService);
+
+  get cluster() {
+    return this.clusters.find(c => c.active) || defaultClusters[0]
+  }
+
+  get clusters() {
+    const items = (this.storage.get<Cluster[]>(STORAGE_KEY_CLUSTERS) || defaultClusters).sort((a, b) => a.name > b.name ? 1 : -1)
+    const cluster = this.storage.get<Cluster>(STORAGE_KEY_CLUSTER) || defaultClusters[0]
+    return items.map(i => ({
+      ...i,
+      active: i.name === cluster.name
+    }))
+  }
+
+  addCluster(cluster: Cluster) {
+    try {
+      new Connection(cluster.endpoint)
+      this.storage.set(STORAGE_KEY_CLUSTERS, [...this.clusters, cluster])
+    } catch (err) {
+      // TODO: Refactor to toast/notification so it's visible to the user
+      console.log(`Error adding cluster: ${err}`)
+    }
+  }
+
+  deleteCluster(cluster: Cluster) {
+    this.storage.set(STORAGE_KEY_CLUSTERS, [...this.clusters.filter(c => c.name !== cluster.name)])
+  }
+
+  setCluster(cluster: Cluster) {
+    this.storage.set(STORAGE_KEY_CLUSTER, cluster)
+  }
+
+  getExplorerUrl(path: string) {
+    return `https://explorer.solana.com/${path}${getClusterUrlParam(this.cluster)}`
+  }
+}
+
+function getClusterUrlParam(cluster: Cluster): string {
+  let suffix = ''
+  switch (cluster.network) {
+    case ClusterNetwork.Devnet:
+      suffix = 'devnet'
+      break
+    case ClusterNetwork.Mainnet:
+      suffix = ''
+      break
+    case ClusterNetwork.Testnet:
+      suffix = 'testnet'
+      break
+    default:
+      suffix = `custom&customUrl=${encodeURIComponent(cluster.endpoint)}`
+      break
+  }
+
+  return suffix.length ? `?cluster=${suffix}` : ''
+}
